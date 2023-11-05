fe81a14b4d8f6f7f3bd3188280400dfcd9f05ae0
FirEmerald <mccaslinwalt@gmail.com>
2023-11-04 22:32:51 -0500

    Improve handling of weathering states
    improved memoized supplier hack
    direct replacement of suppliers on honeycomb item

--- src/main/java/com/firemerald/additionalplacements/common/CommonModEventHandler.java
+++ src/main/java/com/firemerald/additionalplacements/common/CommonModEventHandler.java
@@ -3,6 +3,7 @@ package com.firemerald.additionalplacements.common;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
@@ -10,6 +11,7 @@ import com.firemerald.additionalplacements.AdditionalPlacementsMod;
 import com.firemerald.additionalplacements.block.*;
 import com.firemerald.additionalplacements.block.interfaces.IPlacementBlock;
 import com.firemerald.additionalplacements.datagen.ModelGenerator;
+import com.google.common.base.Suppliers;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
 
@@ -74,7 +76,7 @@ public class CommonModEventHandler
 	@SubscribeEvent
 	public static void init(FMLCommonSetupEvent event)
 	{
-		try
+		try //we need to do this hack because we can't have non-final static fields on interfaces, because Java doesn't let us have nice things. However, it is volatile, and should be replaced when it becomes possible.
 		{
 			Class<?> clazz = Class.forName("com.google.common.base.Suppliers$NonSerializableMemoizingSupplier");
 			Field delegate = clazz.getDeclaredField("delegate");
@@ -83,29 +85,9 @@ public class CommonModEventHandler
 			initialized.setAccessible(true);
 			Field value = clazz.getDeclaredField("value");
 			value.setAccessible(true);
-			Function<BiMap<Block, Block>, BiMap<Block, Block>> withAdditionalStates = oldMap -> {
-				BiMap<Block, Block> newMap = HashBiMap.create(oldMap);
-				oldMap.forEach((b1, b2) -> {
-					if (b1 instanceof IPlacementBlock && b2 instanceof IPlacementBlock)
-					{
-						IPlacementBlock<?> p1 = (IPlacementBlock<?>) b1;
-						IPlacementBlock<?> p2 = (IPlacementBlock<?>) b2;
-						if (p1.hasAdditionalStates() && p2.hasAdditionalStates()) newMap.put(p1.getOtherBlock(), p2.getOtherBlock());
-					}
-				});
-				return newMap;
-			};
 			try
 			{
-				modifyMap(WeatheringCopper.NEXT_BY_BLOCK, WeatheringCopper.PREVIOUS_BY_BLOCK, withAdditionalStates, delegate, initialized, value);
-			}
-			catch (IllegalArgumentException | IllegalAccessException e)
-			{
-				AdditionalPlacementsMod.LOGGER.error("Failed to update WeatheringCopper maps, copper slabs and stairs will weather into vanilla states. Sorry.", e);
-			}
-			try
-			{
-				modifyMap(HoneycombItem.WAXABLES, HoneycombItem.WAX_OFF_BY_BLOCK, withAdditionalStates, delegate, initialized, value);
+				modifyMap(WeatheringCopper.NEXT_BY_BLOCK, WeatheringCopper.PREVIOUS_BY_BLOCK, CommonModEventHandler::addVariants, delegate, initialized, value);
 			}
 			catch (IllegalArgumentException | IllegalAccessException e)
 			{
@@ -114,27 +96,49 @@ public class CommonModEventHandler
 		}
 		catch (ClassNotFoundException | NoSuchFieldException | SecurityException e)
 		{
-			AdditionalPlacementsMod.LOGGER.error("Failed to update WeatheringCopper and HoneycombItem maps, copper slabs and stairs will weather into vanilla states and cannot be waxed. Sorry.", e);
+			AdditionalPlacementsMod.LOGGER.error("Failed to update WeatheringCopper maps, copper slabs and stairs will weather into vanilla states. Sorry.", e);
 		}
+		Supplier<BiMap<Block, Block>> waxables = HoneycombItem.WAXABLES;
+		HoneycombItem.WAXABLES = Suppliers.memoize(() -> addVariants(waxables.get()));
+		HoneycombItem.WAX_OFF_BY_BLOCK = Suppliers.memoize(() -> HoneycombItem.WAXABLES.get().inverse());
+	}
+	
+	public static BiMap<Block, Block> addVariants(Map<Block, Block> oldMap)
+	{
+		BiMap<Block, Block> newMap = HashBiMap.create(oldMap);
+		oldMap.forEach((b1, b2) -> {
+			if (b1 instanceof IPlacementBlock && b2 instanceof IPlacementBlock)
+			{
+				IPlacementBlock<?> p1 = (IPlacementBlock<?>) b1;
+				IPlacementBlock<?> p2 = (IPlacementBlock<?>) b2;
+				if (p1.hasAdditionalStates() && p2.hasAdditionalStates()) newMap.put(p1.getOtherBlock(), p2.getOtherBlock());
+			}
+		});
+		return newMap;
 	}
 
 	public static <T, U> void modifyMap(Supplier<BiMap<T, U>> forwardMemoized, Supplier<BiMap<U, T>> backwardMemoized, Function<BiMap<T, U>, BiMap<T, U>> modify, Field delegate, Field initialized, Field value) throws IllegalArgumentException, IllegalAccessException
 	{
-		@SuppressWarnings("unchecked")
-		com.google.common.base.Supplier<BiMap<T, U>> supplier = (com.google.common.base.Supplier<BiMap<T, U>>) delegate.get(forwardMemoized);
-		if (supplier == null)
+		if (initialized.getBoolean(forwardMemoized)) //already computed
 		{
 			@SuppressWarnings("unchecked")
-			BiMap<T, U> map = (BiMap<T, U>) value.get(forwardMemoized);
-			supplier = () -> map;
+			BiMap<T, U> map = (BiMap<T, U>) value.get(forwardMemoized); //get existing map
+			value.set(forwardMemoized, null); //clear value
+			initialized.setBoolean(forwardMemoized, false); //clear initialized flag
+			delegate.set(forwardMemoized, (com.google.common.base.Supplier<BiMap<T, U>>) () -> modify.apply(map)); //replace with supplier that modifies the existing map
 		}
-		final com.google.common.base.Supplier<BiMap<T, U>> oldSupplier = supplier;
-		delegate.set(forwardMemoized, (com.google.common.base.Supplier<BiMap<T, U>>) () -> modify.apply(oldSupplier.get()));
-		initialized.setBoolean(forwardMemoized, false);
-		value.set(forwardMemoized, null);
-		delegate.set(backwardMemoized, (com.google.common.base.Supplier<BiMap<U, T>>) () -> forwardMemoized.get().inverse());
-		initialized.setBoolean(backwardMemoized, false);
-		value.set(backwardMemoized, null);
+		else
+		{
+			@SuppressWarnings("unchecked")
+			com.google.common.base.Supplier<BiMap<T, U>> forwardSupplier = (com.google.common.base.Supplier<BiMap<T, U>>) delegate.get(forwardMemoized); //get the existing supplier
+			delegate.set(forwardMemoized, (com.google.common.base.Supplier<BiMap<T, U>>) () -> modify.apply(forwardSupplier.get())); //replace with supplier that modifies the result of the existing supplier
+		}
+		if (initialized.getBoolean(backwardMemoized))
+		{
+			value.set(backwardMemoized, null); //clear value
+			initialized.setBoolean(backwardMemoized, false); //clear initialized flag
+		}
+		delegate.set(backwardMemoized, (com.google.common.base.Supplier<BiMap<U, T>>) () -> forwardMemoized.get().inverse()); //replace with supplier that gets the inverse of the forward map
 	}
 
 	@SubscribeEvent
