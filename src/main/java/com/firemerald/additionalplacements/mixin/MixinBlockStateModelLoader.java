9b04ae67057aeebbb745c2e139094909842a9fc7
FirEmerald <mccaslinwalt@gmail.com>
2025-05-21 21:53:11 -0500

    Fix model generation hook mixin
    hopefully:
    fixes #130
    fixes #132
    fixes #112

---
+++ src/main/java/com/firemerald/additionalplacements/mixin/MixinBlockStateModelLoader.java
@@ -0,0 +1,58 @@
+package com.firemerald.additionalplacements.mixin;
+
+import com.firemerald.additionalplacements.block.AdditionalPlacementBlock;
+import com.firemerald.additionalplacements.client.models.PlacementModelState;
+import com.firemerald.additionalplacements.client.models.UnbakedPlacementModel;
+import com.firemerald.additionalplacements.client.models.definitions.StateModelDefinition;
+import com.firemerald.additionalplacements.generation.Registration;
+import com.firemerald.additionalplacements.util.BlockRotation;
+import net.minecraft.client.renderer.block.BlockModelShaper;
+import net.minecraft.client.renderer.block.model.UnbakedBlockStateModel;
+import net.minecraft.client.resources.model.BlockStateModelLoader;
+import net.minecraft.client.resources.model.ModelResourceLocation;
+import net.minecraft.client.resources.model.ModelState;
+import net.minecraft.client.resources.model.UnbakedModel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
+@Mixin(BlockStateModelLoader.class)
+public class MixinBlockStateModelLoader {
+    @Inject(method = "loadBlockStates(Lnet/minecraft/client/resources/model/UnbakedModel;Lnet/minecraft/server/packs/resources/ResourceManager;Ljava/util/concurrent/Executor;)Ljava/util/concurrent/CompletableFuture;", at = @At("RETURN"), cancellable = true)
+    private static void postLoadModels(UnbakedModel model, ResourceManager resourceManager, Executor executor, CallbackInfoReturnable<CompletableFuture<BlockStateModelLoader.LoadedModels>> cir) {
+        cir.setReturnValue(cir.getReturnValue().thenApply(loadedModels -> {
+            Map<ModelResourceLocation, BlockStateModelLoader.LoadedModel> models = loadedModels.models();
+            Registration.forEachCreated(entry -> {
+                AdditionalPlacementBlock<?> block = entry.newBlock();
+                block.getStateDefinition().getPossibleStates().forEach(ourState -> {
+                    ModelResourceLocation ourModelLocation = BlockModelShaper.stateToModelLocation(ourState);
+                    models.computeIfAbsent(ourModelLocation, unused -> {
+                        BlockState theirState = block.getModelState(ourState);
+                        StateModelDefinition modelDefinition = block.getModelDefinition(ourState);
+                        ResourceLocation ourModel = modelDefinition.location(block.getBaseModelPrefix());
+                        ModelState ourModelRotation = PlacementModelState.by(modelDefinition.xRotation().value, modelDefinition.yRotation().value);
+                        ModelResourceLocation theirModelLocation = BlockModelShaper.stateToModelLocation(theirState);
+                        if (models.containsKey(theirModelLocation)) {
+                            UnbakedBlockStateModel theirModel = models.get(theirModelLocation).model();
+                            BlockRotation theirModelRotation = block.getRotation(ourState);
+                            return new BlockStateModelLoader.LoadedModel(
+                                    ourState,
+                                    UnbakedPlacementModel.of(block, ourModel, ourModelRotation, theirModel, theirModelRotation)
+                            );
+                        } else return null;
+                    });
+                });
+            });
+            UnbakedPlacementModel.clearCache();
+           return loadedModels;
+        }));
+    }
+}
