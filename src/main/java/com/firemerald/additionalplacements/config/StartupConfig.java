d511f0e85b14cf775bbfe544300def9d0c81eae2
FirEmerald <mccaslinwalt@gmail.com>
2025-05-21 19:05:39 -0500

    blacklist/whitelist overhaul

--- src/main/java/com/firemerald/additionalplacements/config/StartupConfig.java
+++ src/main/java/com/firemerald/additionalplacements/config/StartupConfig.java
@@ -1,25 +1,22 @@
 package com.firemerald.additionalplacements.config;
 
+import com.firemerald.additionalplacements.config.blocklist.Blocklist;
 import com.firemerald.additionalplacements.generation.GenerationType;
 import com.firemerald.additionalplacements.generation.Registration;
 
 import net.neoforged.neoforge.common.ModConfigSpec;
 
 public class StartupConfig {
-	public final GenerationBlacklist blacklist = new GenerationBlacklist.Builder().build();
+	public final Blocklist enabled = new Blocklist(false, true);
 
 	public StartupConfig(ModConfigSpec.Builder builder) {
         builder.comment("Startup settings").push("startup");
-		builder
-		.comment("Options for controlling which blocks can generate variants of a their type (if one exists).")
-		.push("enabled");
-		blacklist.addToConfig(builder);
-		builder.pop();
+		enabled.addToConfig(builder, "enabled", "Blocklist for controlling which blocks can generate variants of a their type (if one exists).");
         Registration.buildConfig(builder, GenerationType::buildStartupConfig);
 	}
 
 	public void onConfigLoaded() {
-		blacklist.loadListsFromConfig();
+		enabled.loadListsFromConfig();
 		Registration.forEach(GenerationType::onStartupConfigLoaded);
 	}
 }
