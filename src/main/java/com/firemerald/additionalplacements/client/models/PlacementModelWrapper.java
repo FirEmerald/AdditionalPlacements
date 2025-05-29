7260a08eafe050cac5476a0bb2e8aa6b80942e95
FirEmerald <mccaslinwalt@gmail.com>
2025-05-29 15:06:05 -0500

    Block model type split
    Pre-process rotated model/texture options
    Use separate model classes for rotated and retextured models
    Do not generate models for blocks with missing "closest vanilla state" models
    Remove unused code from BlockModelUtils

--- src/main/java/com/firemerald/additionalplacements/client/models/PlacementModelWrapper.java
+++ src/main/java/com/firemerald/additionalplacements/client/models/PlacementModelWrapper.java
@@ -2,7 +2,9 @@ package com.firemerald.additionalplacements.client.models;
 
 import java.util.List;
 
-import org.jetbrains.annotations.NotNull;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraftforge.client.ChunkRenderTypeSet;
 import org.jetbrains.annotations.Nullable;
 
 import net.minecraft.client.renderer.RenderType;
@@ -14,6 +16,8 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.client.model.BakedModelWrapper;
 import net.minecraftforge.client.model.data.ModelData;
 
+import javax.annotation.Nonnull;
+
 public abstract class PlacementModelWrapper extends BakedModelWrapper<BakedModel> {
 
     public PlacementModelWrapper(BakedModel originalModel)
@@ -22,12 +26,29 @@ public abstract class PlacementModelWrapper extends BakedModelWrapper<BakedModel
     }
 
     @Override
-    public List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction side, RandomSource rand)
-    {
-    	return this.getQuads(state, side, rand, null, null);
+    public abstract List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction side, RandomSource rand);
+
+    @Override
+    public abstract List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction side, RandomSource rand, @Nullable ModelData extraData, @Nullable RenderType renderType);
+
+    @Override
+    public boolean useAmbientOcclusion(BlockState state) {
+        return originalModel.useAmbientOcclusion(BlockModelUtils.getModeledState(state));
     }
 
-    @NotNull
     @Override
-    public abstract List<BakedQuad> getQuads(@Nullable BlockState state, @Nullable Direction side, @NotNull RandomSource rand, @Nullable ModelData extraData, @Nullable RenderType renderType);
+    public boolean useAmbientOcclusion(BlockState state, RenderType renderType) {
+        return originalModel.useAmbientOcclusion(BlockModelUtils.getModeledState(state), renderType);
+    }
+
+    @Nonnull
+    @Override
+    public ModelData getModelData(@Nonnull BlockAndTintGetter level, @Nonnull BlockPos pos, @Nonnull BlockState state, @Nonnull ModelData modelData) {
+        return originalModel.getModelData(level, pos, BlockModelUtils.getModeledState(state), modelData);
+    }
+
+    @Override
+    public ChunkRenderTypeSet getRenderTypes(@Nonnull BlockState state, @Nonnull RandomSource rand, @Nonnull ModelData data) {
+        return originalModel.getRenderTypes(BlockModelUtils.getModeledState(state), rand, data);
+    }
 }
